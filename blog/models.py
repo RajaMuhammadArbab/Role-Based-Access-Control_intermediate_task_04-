07c87ece4939a4ce761d9d09c3c3e9d5bbcebf89
Raja Muhammad Arbab <rajaarbabthakur1@gmail.com>
2025-08-19 12:10:11 +0500

    Create models.py

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from accounts.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)  # Soft delete
+
+    def __str__(self):
+        return self.title
